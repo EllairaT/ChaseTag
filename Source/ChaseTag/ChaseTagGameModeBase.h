5838d67cc9bc32a25e9d0118022f1ff1d9b34b46
Ellaira Torio <ellaira523@gmail.com>
2023-12-20 00:48:39 +0800

    Initial commit

---
+++ Source/ChaseTag/ChaseTagGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChaseTagGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHASETAG_API AChaseTagGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
